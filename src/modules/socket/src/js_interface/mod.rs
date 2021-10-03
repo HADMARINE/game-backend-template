a0c33376045f935789a22fb753ea3a9d570ea2c8
hadmarine <tsx@kakao.com>
2021-10-03 13:47:52 +0900

    WIP

---
+++ src/modules/socket/src/js_interface/mod.rs
@@ -0,0 +1,9 @@
+pub struct JsInterface {}
+
+impl JsInterface {
+    pub fn new() -> Self {
+        JsInterface {}
+    }
+
+    pub fn close_room(&self) -> Result<()> {}
+}
--- src/modules/socket/src/lib.rs
+++ src/modules/socket/src/lib.rs
@@ -1,11 +1,16 @@
 use neon::prelude::*;
 mod error;
+mod js_interface;
 mod socket_instance;
 mod util;
 
 fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("hello node"))
 }
+
+fn create_room(mut cx: FunctionContext) -> JsResult<JsObject> {}
+// fn close_room
+// fn
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
