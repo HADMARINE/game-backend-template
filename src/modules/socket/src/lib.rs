7035b5975b8a1dfef4146753b119e1923f0897ed
hadmarine <tsx@kakao.com>
2021-10-08 17:58:37 +0900

    Finished exposing rust function to js instance

--- src/modules/socket/src/lib.rs
+++ src/modules/socket/src/lib.rs
@@ -1,5 +1,7 @@
 use std::{
+    cell::RefCell,
     collections::HashMap,
+    rc::Rc,
     sync::{Arc, RwLock},
 };
 
@@ -9,7 +11,9 @@ mod js_interface;
 mod socket_instance;
 mod util;
 
-use socket_instance::{QuickSocketInstance, TcpChannelCreatePreferences};
+use socket_instance::{
+    QuickSocketInstance, TcpChannelCreatePreferences, UdpChannelCreatePreferences,
+};
 
 static INSTANCE: Arc<RwLock<QuickSocketInstance>> = QuickSocketInstance::new();
 
@@ -32,6 +36,8 @@ fn create_tcp_channel(mut cx: FunctionContext) -> JsResult<JsObject> {
 
     drop(write_locked);
 
+    let rc_cx = Rc::new(RefCell::from(cx));
+
     let interface = js_interface::JsInterface::new(
         *handler,
         match match channel.instance.read() {
@@ -45,28 +51,78 @@ fn create_tcp_channel(mut cx: FunctionContext) -> JsResult<JsObject> {
         },
         HashMap::new(),
         channel,
+        rc_cx,
     );
 
-    // let boxed_interface = cx.boxed(interface);
-
-    let mut return_object = cx.empty_object();
-    return_object.set(&mut cx, "port", cx.number(interface.addr.port()));
+    let mut return_object = interface.cx.borrow().empty_object();
     return_object.set(
-        &mut cx,
+        interface.cx.get_mut(),
+        "port",
+        interface.cx.borrow().number(interface.addr.port()),
+    );
+    return_object.set(
+        interface.cx.get_mut(),
         "socket_handler",
-        JsFunction::new(&mut cx, js_interface::socket_data_handler)?,
+        JsFunction::new(interface.cx.get_mut(), js_interface::socket_data_handler)?,
     );
 
     Ok(return_object)
 }
 
 fn create_udp_channel(mut cx: FunctionContext) -> JsResult<JsObject> {
-    // let preferences
+    let preferences = match UdpChannelCreatePreferences::from_jsobj(&mut cx, cx.argument(0)?) {
+        Ok(v) => v,
+        Err(_) => return Err(Throw),
+    }; // Preferences
+
+    let handler: Handle<JsFunction> = cx.argument(1)?;
+
+    let write_locked = match INSTANCE.write() {
+        Ok(v) => v,
+        Err(_) => return Err(Throw),
+    };
+    let channel = match write_locked.create_udp_channel(|_| {}, preferences) {
+        Ok(v) => v,
+        Err(_) => return Err(Throw),
+    };
+
+    drop(write_locked);
+
+    let rc_cx = Rc::new(RefCell::from(cx));
+
+    let interface = js_interface::JsInterface::new(
+        *handler,
+        match match channel.instance.read() {
+            Ok(v) => v,
+            Err(_) => return Err(cx.throw_error("instance init invalid")?),
+        }
+        .local_addr()
+        {
+            Ok(v) => v,
+            Err(_) => return Err(cx.throw_error("instance init invalid")?),
+        },
+        HashMap::new(),
+        channel,
+        rc_cx,
+    );
+
+    let mut return_object = interface.cx.borrow().empty_object();
+    return_object.set(
+        interface.cx.get_mut(),
+        "port",
+        interface.cx.borrow().number(interface.addr.port()),
+    );
+    return_object.set(
+        interface.cx.get_mut(),
+        "socket_handler",
+        JsFunction::new(interface.cx.get_mut(), js_interface::socket_data_handler)?,
+    );
+
+    Ok(return_object)
 }
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    let instance = QuickSocketInstance::new();
     cx.export_function("createVacantTcpChannel", create_tcp_channel)?;
     cx.export_function("createUdpChannel", create_udp_channel)?;
     Ok(())
