1adb608f51564e2abf68dc2742590b216a69c1c8
hadmarine <tsx@kakao.com>
2021-10-10 21:45:59 +0900

    Created d.ts

--- src/modules/socket/index.d.ts
+++ src/modules/socket/index.d.ts
@@ -1,1 +1,32 @@
-// export function create_tcp_channel
+export type BoxedJsInterface = unknown;
+export type JsHandlerFunction = (
+  event: string,
+  data: Record<string, any>,
+) => void;
+export interface JsInterface {
+  port: number;
+  socketHandler: (
+    interface: BoxedJsInterface,
+    data: Record<string, any>,
+  ) => void;
+}
+
+export namespace ChannelCreatePreferences {
+  interface Tcp {
+    deleteClientWhenClosed: boolean;
+    concurrent: boolean;
+  }
+
+  interface Udp {
+    deleteClientWhenClosed: boolean;
+  }
+}
+
+export function create_tcp_channel(
+  pref: ChannelCreatePreferences.Tcp,
+  handler: JsHandlerFunction,
+): JsInterface;
+export function create_udp_channel(
+  pref: ChannelCreatePreferences.Udp,
+  handler: JsHandlerFunction,
+): JsInterface;
